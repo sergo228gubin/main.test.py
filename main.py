d446303654c5d1de9f0ac092e8685b843716656a
sergo228gubin <145856152+sergo228gubin@users.noreply.github.com>
2023-09-30 18:44:34 +0300

    asdfghjkl;
    asdfghjkl;

---
+++ main.py
@@ -0,0 +1,26 @@
+print("Hi kiberOne")
+str1 = "22"
+print(type(str1))
+int1 = 2
+print(type(int1))
+float1 = 3.5
+print(type(float1))
+l = [1,2,3,4]
+print(type(l))
+
+print(type(1+2.0))
+
+print(type(2.4+1))
+
+print(type(2.0 * 1))
+
+Jon_age = 20
+mom_age = 46
+paul = (mom_age + Jon_age)/6
+
+def my_function(x):
+  return 5 * x
+
+print(my_function(5))
+print(my_function(9))
+
